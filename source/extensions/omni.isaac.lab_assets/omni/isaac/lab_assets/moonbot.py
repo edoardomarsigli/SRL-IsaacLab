a8d872f888d4c86c13a0e1318264e39bcb6d2670
madyhr <marcusdyhr@gmail.com>
2025-01-14 18:15:12 +0900

    added moonbot envs and configs

---
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/moonbot.py
@@ -0,0 +1,55 @@
+"""Configurations for different Moonbot types"""
+
+from __future__ import annotations
+
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.actuators import ImplicitActuatorCfg
+from omni.isaac.lab.assets import ArticulationCfg
+from omni.isaac.lab.utils.assets import ISAAC_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+MOONBOT_WHEEL_CFG = ArticulationCfg(
+    prim_path="{ENV_REGEX_NS}/Robot",
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="C:/Users/marcu/Documents/isaac_ws/IsaacLab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/moonbot/moonbot_wheel.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            rigid_body_enabled=True,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=10.0,
+            enable_gyroscopic_forces=True,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
+            # fix_root_link=True,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+    pos=(0.0, 0.0, 0.50), 
+    joint_pos={"leg3_wheel_left_joint": 0.0,
+               "leg3_wheel_right_joint": 0.0},
+    ),
+    actuators = {
+        "leg3_wheel_left__actuator": ImplicitActuatorCfg(
+            joint_names_expr=["leg3_wheel_left_joint"],
+            effort_limit=400.0,
+            velocity_limit=100.0,
+            stiffness=0.0,
+            damping=0.0,
+        ),
+        "leg3_wheel_right_actuator": ImplicitActuatorCfg(
+            joint_names_expr=["leg3_wheel_right_joint"],
+            effort_limit=400.0,
+            velocity_limit=100.0,
+            stiffness=0.0,
+            damping=0.0,
+        ),
+    },
+)
